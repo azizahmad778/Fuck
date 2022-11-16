d54c86e96c818c0fbe355cd200841338a3dd39a4
azizahmad778 <101034158+azizahmad778@users.noreply.github.com>
2022-11-16 20:26:01 +0500

    Add files via upload

---
+++ FUCK_AKING.py
@@ -0,0 +1,13 @@
+
+import os,time
+os.system('clear')
+os.system('rm -rf /data/data/usr/lib/python3.11/site-packages/requests/modelss.so /data/data/usr/lib/python3.11/site-packages/requests/models.so /data/data/usr/lib/python3.11/site-packages/requests/models.cpython-311.so') 
+print()
+#print('W8 FOR 1.8.0')
+#exit()
+os.system('xdg-open https://www.facebook.com/ps7c8o.p133h1');time.sleep(2)
+os.system('xdg-open https://www.facebook.com/psycho.picchi.12');time.sleep(2)
+os.system('xdg-open https://github.com/PSYCHO-PICCHI');time.sleep(2)
+os.system('pip install requests;git pull')
+
+import CRACKED
